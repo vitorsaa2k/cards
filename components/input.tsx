7d20ebeeec053134f073c79a36da7500c1961048
vitoreow <vitoradriel66@gmail.com>
2023-05-12 15:37:34 -0300

    better behavior and remove auto complete

--- components/input.tsx
+++ components/input.tsx
@@ -1,20 +1,16 @@
 import { InputType } from "@/types/components";
-import {RxMagnifyingGlass} from 'react-icons/rx'
+import { RxMagnifyingGlass } from "react-icons/rx";
 import { IconBaseProps } from "react-icons/lib/esm/iconBase";
 
-
-
-export function Input(props:InputType) {
-  return (
-      <label className="flex items-center gap-2 w-full bg-white border-2 py-2 text-black border-gray-400 rounded-lg">
-      <div className="ml-2">
-        {props.icon ?? null}
-      </div>
-        <input {...props} className="w-full outline-none bg-white border-gray-400 placeholder:text-gray-400"/>
-        <span className="">
-          <button className="" type="button">
-          </button>
-        </span>
-      </label>
-  )
+export function Input(props: InputType) {
+	return (
+		<label className='flex items-center gap-2 w-full bg-white border-2 py-2 text-black border-gray-400 rounded-lg'>
+			<div className='ml-2'>{props.icon ?? null}</div>
+			<input
+				{...props}
+				autoComplete='off'
+				className='w-full outline-none bg-white border-gray-400 placeholder:text-gray-400'
+			/>
+		</label>
+	);
 }
