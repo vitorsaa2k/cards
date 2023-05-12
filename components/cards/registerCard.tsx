c69cf9270618311b052978a1ec3639344a0eac21
vitoreow <vitoradriel66@gmail.com>
2023-05-12 16:18:12 -0300

    close modal on mutation success

--- components/cards/registerCard.tsx
+++ components/cards/registerCard.tsx
@@ -1,20 +1,21 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { Input } from "../input";
-import { Button } from "../common/button";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { addCard } from "@/actions/cards";
-import { CardType } from "@/types/api";
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Input } from '../input';
+import { Button } from '../common/button';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { addCard } from '@/actions/cards';
+import { CardType } from '@/types/api';
 
 export function RegisterCard({ toggle }: { toggle: () => void }) {
 	const queryClient = useQueryClient();
 	const [form, setForm] = useState({
-		name: "",
-		cpf: "",
+		name: '',
+		cpf: '',
 	});
 
 	const mutation = useMutation((card: CardType) => addCard(card), {
 		onSuccess: () => {
-			queryClient.invalidateQueries(["cards"]);
+			queryClient.invalidateQueries(['cards']);
+			toggle();
 		},
 	});
 
@@ -25,16 +26,16 @@ export function RegisterCard({ toggle }: { toggle: () => void }) {
 	}
 
 	function formatCpf(cpf: string) {
-		return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+		return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 	}
 
 	function handleInputs(e: ChangeEvent<HTMLInputElement>) {
 		let { name, value } = e.currentTarget;
-		if (name === "cpf" && value.length > 0) {
+		if (name === 'cpf' && value.length > 0) {
 			numbersRegex.test(value) ? (value = value) : (value = form.cpf);
 			value = formatCpf(value);
-		} else if (name === "cpf" && value.length === 0) {
-			value = value = "";
+		} else if (name === 'cpf' && value.length === 0) {
+			value = value = '';
 		}
 		setForm(prevForm => ({
 			...prevForm,
