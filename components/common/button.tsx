1c0aa1439093ee1226e9d93730bba0810633c841
vitoreow <vitoradriel66@gmail.com>
2023-05-17 15:22:59 -0300

    fix common button props

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -3,8 +3,8 @@ import { ButtonType } from "@/types/components";
 export function Button(props: ButtonType) {
 	return (
 		<button
+			className="w-full flex items-center justify-center bg-slate-500 text-white py-2 rounded-lg"
 			{...props}
-			className="w-full text-center bg-slate-500 text-white py-2 rounded-lg"
 		>
 			{props.children}
 		</button>
