a339373d43e8a849b95b498741cbedce9ad04c49
Vitor Adriel <vitoradriel66@gmail.com>
2023-05-18 12:47:46 +0000

    rework on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-import { useState } from 'react'
-import axios from 'axios'
+import { Button } from '@/components/common/button'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
-
-  async function submit() {
-    try {
-      console.log(email, name, password)
-      axios.post('/api/register', {email, name, password}).then(response => console.log(response))
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   
   return (
     <>
-      <Link href={'/cards'}>Cards</Link>
-      <div>
-        <label>
-          email
-        <input onChange={(e) => setEmail(e.currentTarget.value)}></input>
-        </label>
-        <label>
-          name
-        <input onChange={(e) => setName(e.currentTarget.value)}></input>
-        </label>
-        <label>
-          password
-        <input onChange={(e) => setPassword(e.currentTarget.value)}></input>
-        </label>
-
-        <button onClick={submit}>submit</button>
-      </div>
+      <main className='w-screen h-screen flex justify-center items-center'>
+          <section>
+            <div className='bg-slate-500 p-4 rounded flex flex-col items-center'>
+              Seja bem-vindo à plataforma de consulta do cliente!
+              <Button>Já possuo uma conta</Button>
+              <Button>Criar conta</Button>
+              <Button >Pagina principal</Button>
+            </div>
+          </section>
+      </main>
     </>
   )
 }
