d4fca670d5e9251bf04647592609be27a75fc23f
vitoreow <vitoradriel66@gmail.com>
2023-05-19 21:39:45 -0300

    h1 on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import { Button } from '@/components/common/button'
-
-
 export default function Home() {
-
-
-  
-  return (
-    <>
-      <main className='w-screen h-screen flex justify-center items-center'>
-          <section>
-            <div className='bg-slate-500 p-4 rounded flex flex-col items-center'>
-              Seja bem-vindo à plataforma de consulta do cliente!
-              <Button>Já possuo uma conta</Button>
-              <Button>Criar conta</Button>
-              <Button >Pagina principal</Button>
-            </div>
-          </section>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<main className="">
+				<div className="p-14">
+					<h1 className="text-6xl">Início</h1>
+				</div>
+				<section className="w-screen h-screen flex justify-center items-center"></section>
+			</main>
+		</>
+	);
 }
