dcaec8bd0664d716493ec400d74a63c3a30d0988
vitoreow <vitoradriel66@gmail.com>
2023-05-25 16:24:30 -0300

    test fix api on deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "/:path*",
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
