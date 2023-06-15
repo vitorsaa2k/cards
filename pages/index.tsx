bcbcd5fc95d0e102026c1dd3a706bf90d390a5b5
vitoreow <vitoradriel66@gmail.com>
2023-06-15 19:35:24 -0300

    remove horizontal scrollbar from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ export default function Home() {
 				<div className="p-14">
 					<h1 className="text-6xl">Início</h1>
 				</div>
-				<section className="w-screen h-screen flex justify-center items-center"></section>
 			</main>
 		</>
 	);
