fdc0a9ee997ee48063fe7d55eabc22c4aa26c80f
vitoreow <vitoradriel66@gmail.com>
2023-06-20 19:00:18 -0300

    fix dropDown position

--- components/nav/dropDown.tsx
+++ components/nav/dropDown.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 export function DropDown({ session }: { session: Session }) {
 	return (
-		<div className="absolute translate-y-[30px] bg-black/25 backdrop-blur-sm flex flex-col animate-dropDown p-3 rounded-md">
+		<div className="absolute translate-y-[30px] translate-x-[-20px] bg-black/25 backdrop-blur-sm flex flex-col animate-dropDown p-3 rounded-md">
 			<p>{session.user?.email}</p>
 			<div>
 				<Button onClick={() => signOut()}>Sair</Button>
