b8814d75a61d002277983896e972fcb8a9659760
vitoreow <vitoradriel66@gmail.com>
2023-06-20 18:48:37 -0300

    handle cpf and email registration

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,37 +1,78 @@
 import prismadb  from '@/libs/prismadb';
 import bcrypt from 'bcrypt'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { generateRandomKey } from '@/actions/common';
+import { redirect } from 'next/navigation'
+
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method !== 'POST') {
     return res.status(405).end()
   }
+  const {email, cpf, name, password} = req.body.user
 
-  try {
-    const {email, name, password} = req.body
-
-    const existingUser = await prismadb.user.findUnique({
-      where: {
-        email,
+  if(cpf.toString().length <= 0) {
+    try {
+  
+      const existingUser = await prismadb.user.findUnique({
+        where: {
+          email,
+        }
+      })
+      if(existingUser) {
+        return res.status(422).json({email: 'Email taken'})
       }
-    })
-    if(existingUser) {
-      return res.status(422).json({email: 'Email taken'})
+  
+      const hashedPassword = await bcrypt.hash(password, 12)
+      const user = await prismadb.user.create({
+        data: {
+          email,
+          name,
+          hashedPassword,
+          emailVerified: new Date(),
+          cpf: generateRandomKey(),
+        }
+      }).then(data => {
+        res.status(200).json(data) 
+        return redirect('/cards')
+      })
+  
+    } catch (error) {
+      console.log(error)
+      return res.status(400).end()
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const user = await prismadb.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-        image: '',
-        emailVerified: new Date()
+  } else {
+    try {
+  
+      const existingUser = await prismadb.user.findUnique({
+        where: {
+          cpf,
+        }
+      })
+      if(existingUser) {
+        return res.status(422).json({cpf: 'esse cpf já esta sendo usado'})
       }
-    }).then(data => res.status(200).json(data))
-
-  } catch (error) {
-    console.log(error)
-    return res.status(400).end()
+      const formatedCpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+  
+      const hashedPassword = await bcrypt.hash(password, 12)
+      const user = await prismadb.user.create({
+        data: {
+          email: generateRandomKey(),
+          name,
+          hashedPassword,
+          emailVerified: undefined,
+          cpf: formatedCpf,
+        }
+      }).then(data => {
+        res.status(200).json(data) 
+        return redirect('/cards')
+      })
+  
+    } catch (error) {
+      console.log(error)
+      return res.status(400).end()
+    }
   }
+
 }
