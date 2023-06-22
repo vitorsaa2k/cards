04e206fea43dc36e9bcf5060d9d00ea9d17863bf
vitoreow <vitoradriel66@gmail.com>
2023-06-22 18:58:22 -0300

    use react-spring library to animate the user dropDown

--- components/nav/userName.tsx
+++ components/nav/userName.tsx
@@ -2,9 +2,16 @@ import { UserNameType } from "@/types/components";
 import { useState } from "react";
 import { VscTriangleDown } from "react-icons/vsc";
 import { DropDown } from "./dropDown";
+import { animated, useTransition } from "@react-spring/web";
 
 export function UserName({ session }: UserNameType) {
 	const [isShowing, setIsShowing] = useState(false);
+	const transition = useTransition(isShowing, {
+		from: { opacity: 0, y: 0 },
+		enter: { opacity: 1, y: 10 },
+		leave: { opacity: 0, y: 0 },
+		config: { mass: 1, tession: 300, friction: 20 },
+	});
 	return (
 		<>
 			<div
@@ -14,7 +21,13 @@ export function UserName({ session }: UserNameType) {
 				<p>{session.user?.name}</p>
 				<VscTriangleDown size={24} />
 			</div>
-			{isShowing ? <DropDown session={session} /> : null}
+			{transition((style, item) =>
+				item ? (
+					<animated.div style={style}>
+						<DropDown session={session} />
+					</animated.div>
+				) : null
+			)}
 		</>
 	);
 }
